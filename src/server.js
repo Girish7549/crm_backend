cadafb6dfecce2d5b215c0efdecb72224f92f1a9
Girish Kumar <girish@adzdrio.com>
2025-08-23 15:09:14 +0530

    23/8/25 03:09 PM

--- src/server.js
+++ src/server.js
@@ -155,7 +155,8 @@ io.on("connection", (socket) => {
 app.set("io", io);
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 app.options("*", cors());
 app.use("/api", userRoutes);
